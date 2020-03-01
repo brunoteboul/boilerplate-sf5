65625a339bf00fef6410783289013988bec7aa4f
Bruno Teboul <bruno.teboul@gmail.com>
2020-03-01 14:50:38 +0100

    Add cors checker controller

--- app/symfony/src/Controller/CorsController.php
+++ app/symfony/src/Controller/CorsController.php
@@ -6,15 +6,29 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CorsController extends AbstractController
 {
     /**
-     * @Route("/cors/", name="check_cors")
+     * @Route("/cors/", name="cors_check")
      */
     public function index(): ?Response
     {
         return $this->render('/util/cors.html.twig');
+    }
+
+    /**
+     * @Route("/cors/request/", name="cors_request")
+     */
+    public function bench(): JsonResponse
+    {
+        return $this->json([
+            ['id' => 1, 'username' => 'Sofiane'],
+            ['id' => 2, 'username' => 'Patrice'],
+            ['id' => 3, 'username' => 'Jerome'],
+            ['id' => 3, 'username' => 'Eric'],
+        ]);
     }
 }
