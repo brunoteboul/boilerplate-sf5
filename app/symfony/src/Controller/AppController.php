c24cea471b0ce1107a5c2f24827f59d1f5d36b2b
Bruno Teboul <bruno.teboul@gmail.com>
2020-03-01 23:23:36 +0100

    Fix serializer use

--- app/symfony/src/Controller/AppController.php
+++ app/symfony/src/Controller/AppController.php
@@ -10,7 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use function json_encode;
 
 class AppController extends AbstractController
@@ -26,7 +28,11 @@ class AppController extends AbstractController
         if (! empty($user)) {
             $userClone = clone $user;
             $userClone->setPassword('');
-            $data = $this->serializer->serialize($userClone, JsonEncoder::FORMAT);
+            $encoders = [new JsonEncoder()];
+            $normalizers = [new ObjectNormalizer()];
+
+            $serializer = new Serializer($normalizers, $encoders);
+            $data = $serializer->serialize($userClone, JsonEncoder::FORMAT);
         }
 
         return $this->render('/app.html.twig', [
